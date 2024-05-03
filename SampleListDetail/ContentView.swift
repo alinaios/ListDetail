64015609ddb828668bf4f1702cae174f67dc7c29
alinaios <85871814+alinaios@users.noreply.github.com>
2024-05-03 14:25:08 +0200

    Initial Commit

---
+++ SampleListDetail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleListDetail
+//
+//  Created by AH on 2024-05-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleListDetail/SampleListDetailApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleListDetailApp.swift
+//  SampleListDetail
+//
+//  Created by AH on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct SampleListDetailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
